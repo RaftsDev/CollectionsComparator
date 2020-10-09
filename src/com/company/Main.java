892a5166208710016ec61e507b6ad690f5289755
Ralph F <RaftsDev@gmail.com>
2020-10-09 12:06:55 -0400

    Feat: Code in Main()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Theater theater = new Theater("MSG",8,12);
+	if(theater.reserveSeat("B07")){
+        System.out.println("Seat reserved for you");
+    }else{
+        System.out.println("Seat already reserved for somebody else");
+    }
     }
 }
