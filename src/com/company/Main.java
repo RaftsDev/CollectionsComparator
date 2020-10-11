ef0735f154af44e7a910a4446d97eebec6c71755
Ralph F <RaftsDev@gmail.com>
2020-10-10 22:57:27 -0400

    Chore: Clean code.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,7 +31,6 @@ public class Main {
 		Collections.reverse(reverseSeats);
 		printList(reverseSeats);
 
-
 		List<Theater.Seat> priceSeats = new ArrayList<>(theater.getSeats());
 		priceSeats.add(theater.new Seat("B00", 13.00));
 		priceSeats.add(theater.new Seat("A00", 13.00));
@@ -40,7 +39,6 @@ public class Main {
 		Collections.sort(priceSeats, Theater.PRICE_ORDER);
 		printList(priceSeats);
 	}
-
 
 	public static void printList(List<Theater.Seat> list) {
 		for(Theater.Seat seat : list) {
