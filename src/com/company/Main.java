2a13b98c7e8ac851d7195cabf618f29b01544c22
Ralph F <RaftsDev@gmail.com>
2020-10-10 22:11:55 -0400

    Feat: Change type to Collection.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
 	Theater theater = new Theater("MSG",8,12);
-	List<>
 	printList(theater.getSeats());
 	if(theater.reserveSeat("B07")){
         System.out.println("Seat reserved for you");
@@ -22,7 +23,7 @@ public class Main {
 
     }
 
-    public static void printList(List<Theater.Seat> list){
+    public static void printList(Collection<Theater.Seat> list){
 		System.out.println("*****************************");
     	for(Theater.Seat seat : list){
 			System.out.println(seat.getSeatNumber());
